fa5488bd6af0aab4a9c365e36aac38266381ec47
jake.k <jake.k@hpcnt.com>
2022-03-07 12:47:22 +0900

    Initial Commit

---
+++ ExMultiSectionCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExMultiSectionCollectionView
+//
+//  Created by Jake.K on 2022/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
